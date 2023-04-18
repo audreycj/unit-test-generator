7b467c41397c8d27a1d652f8c2363c844bcc0110
Audrey Carmel Jay J. Nanual <68633976+audreycj@users.noreply.github.com>
2023-04-18 14:16:10 +0800

    Initial commit

---
+++ src/main/java/com/audreynanual/unittestgenerator/UnitTestGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.audreynanual.unittestgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitTestGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnitTestGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/audreynanual/unittestgenerator/UnitTestGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.audreynanual.unittestgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnitTestGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
