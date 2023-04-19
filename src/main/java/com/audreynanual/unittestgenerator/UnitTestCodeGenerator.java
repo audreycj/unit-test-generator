a303d6b64101bb76ff4aee043e5217b9b697611b
Audrey Carmel Jay J. Nanual <68633976+audreycj@users.noreply.github.com>
2023-04-19 11:04:24 +0800

    refactor: rename class

--- src/main/java/com/audreynanual/unittestgenerator/UnitTestCodeGenerator.java
+++
@@ -1,8 +0,0 @@
-// This class generates test code based on the input parameters and expected output values.
-// It can use a template or a code generator to create the test code.
-
-package com.audreynanual.unittestgenerator;
-
-public class UnitTestCodeGenerator {
-    
-}
