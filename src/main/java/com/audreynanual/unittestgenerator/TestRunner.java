c38bc5cef4564bb99073bdfb3364363b3e1710d6
Audrey Carmel Jay J. Nanual <68633976+audreycj@users.noreply.github.com>
2023-04-19 13:36:24 +0800

    feat: remove files

--- src/main/java/com/audreynanual/unittestgenerator/TestRunner.java
+++
@@ -1,9 +0,0 @@
-// This file will contain the logic for running the generated tests and reporting the results to the user.
-// It will invoke the appropriate testing framework's API to run the generated test cases and will report the results back to the user in a clear and concise manner.
-// This class may also handle any errors or exceptions that occur during the test run, providing helpful messages to the user if necessary.
-
-package com.audreynanual.unittestgenerator;
-
-public class TestRunner {
-    
-}
