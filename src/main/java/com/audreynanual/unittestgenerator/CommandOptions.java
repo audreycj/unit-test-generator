ff086b30d2d54ec5c5a39777c233c34ffda68b34
Audrey Carmel Jay J. Nanual <68633976+audreycj@users.noreply.github.com>
2023-04-19 11:53:23 +0800

    feat: add base functionality

--- src/main/java/com/audreynanual/unittestgenerator/CommandOptions.java
+++ src/main/java/com/audreynanual/unittestgenerator/CommandOptions.java
@@ -5,6 +5,57 @@
 
 package com.audreynanual.unittestgenerator;
 
-public class Command {
-    
+import picocli.CommandLine.Option;
+
+
+// In this file, we define a CommandOptions class with several @Option annotations that correspond to the
+// various command-line options we defined in the @Command annotation in the Main class. 
+public class CommandOptions {
+
+    // The following fields will define the command-line options and parameters that the user can specify when running the application.
+    @Option(names = {"-f", "--function"}, required = true, description = "Name of function or module to be tested")
+    private String functionName;
+
+    @Option(names = {"-i", "--input"}, required = true, description = "Input parameter(s) for function or module (comma-separated)")
+    private String inputParameters;
+
+    @Option(names = {"-o", "--output"}, required = true, description = "Expected output(s) for function or module (comma-separated)")
+    private String expectedOutputs;
+
+    @Option(names = {"-t", "--testframework"}, defaultValue = "JUnit5", description = "Testing framework to use (default: JUnit5)")
+    private String testFramework;
+
+    // The ComandOptions class also has getters and setters for each of the options,
+    // which will allow us to access the option values from the TestGenerator class.
+    public String getFunctionName() {
+        return functionName;
+    }
+
+    public void setFunctionName(String functionName) {
+        this.functionName = functionName;
+    }
+
+    public String getInputParameters() {
+        return inputParameters;
+    }
+
+    public void setInputParameters(String inputParameters) {
+        this.inputParameters = inputParameters;
+    }
+
+    public String getExpectedOutputs() {
+        return expectedOutputs;
+    }
+
+    public void setExpectedOutputs(String expectedOutputs) {
+        this.expectedOutputs = expectedOutputs;
+    }
+
+    public String getTestFramework() {
+        return testFramework;
+    }
+
+    public void setTestFramework(String testFramework) {
+        this.testFramework = testFramework;
+    }
 }
