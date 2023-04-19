0d3d8cdaf38764682398e379832bbe5c600234a7
Audrey Carmel Jay J. Nanual <68633976+audreycj@users.noreply.github.com>
2023-04-19 11:01:05 +0800

    refactor: rename class

--- src/main/java/com/audreynanual/unittestgenerator/CommandLineInterface.java
+++
@@ -1,8 +0,0 @@
-// This class handles command-line interface parsing using the Picocli library.
-// It defines the different command-line options and arguments that the user can provide.
-
-package com.audreynanual.unittestgenerator;
-
-public class CommandLineInterface {
-    
-}
