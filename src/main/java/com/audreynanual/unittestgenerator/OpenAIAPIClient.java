cef9f94df580db585fed6590b788d7ffae5c224a
Audrey Carmel Jay J. Nanual <68633976+audreycj@users.noreply.github.com>
2023-04-19 09:26:36 +0800

    docs: add description of the file

--- src/main/java/com/audreynanual/unittestgenerator/OpenAIAPIClient.java
+++ src/main/java/com/audreynanual/unittestgenerator/OpenAIAPIClient.java
@@ -1,3 +1,6 @@
+// This class encapsulates the logic for calling the OpenAI API endpoints and processing the responses.
+// It can use the OpenAI Java SDK or create its own HTTP client to make the API requests.
+
 package com.audreynanual.unittestgenerator;
 
 public class OpenAIAPIClient {
