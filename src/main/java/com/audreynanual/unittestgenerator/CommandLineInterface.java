6af24109f532f8b331dd2abc43a559a84e10c637
Audrey Carmel Jay J. Nanual <68633976+audreycj@users.noreply.github.com>
2023-04-19 09:23:13 +0800

    docs: add description of the file

--- src/main/java/com/audreynanual/unittestgenerator/CommandLineInterface.java
+++ src/main/java/com/audreynanual/unittestgenerator/CommandLineInterface.java
@@ -1,3 +1,6 @@
+// This class handles command-line interface parsing using the Picocli library.
+// It defines the different command-line options and arguments that the user can provide.
+
 package com.audreynanual.unittestgenerator;
 
 public class CommandLineInterface {
