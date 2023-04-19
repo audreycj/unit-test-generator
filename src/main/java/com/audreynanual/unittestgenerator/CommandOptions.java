5f758d10344418343814bbb9648e6258ddbd28ad
Audrey Carmel Jay J. Nanual <68633976+audreycj@users.noreply.github.com>
2023-04-19 11:25:31 +0800

    docs: update file description

---
+++ src/main/java/com/audreynanual/unittestgenerator/CommandOptions.java
@@ -0,0 +1,10 @@
+// This file will define the command structure of the CLI application.
+// It will use the Picocli library to define the options and parameters that the user can specify when running the application.
+// For example, the user may specify the function or module to be tested, the testing framework to use, and the input and output values for each test case.
+// The Command class will define the structure of the command, and Picocli will handle the parsing of the command-line arguments.
+
+package com.audreynanual.unittestgenerator;
+
+public class Command {
+    
+}
