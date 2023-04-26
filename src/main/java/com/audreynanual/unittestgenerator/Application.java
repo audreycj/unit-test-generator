4f001cee0613c9a84faa1b92cfc053ec5da0053b
Audrey Carmel Jay J. Nanual <68633976+audreycj@users.noreply.github.com>
2023-04-26 15:24:51 +0800

    feat: create request object to send to the OpenAI API

--- src/main/java/com/audreynanual/unittestgenerator/Application.java
+++ src/main/java/com/audreynanual/unittestgenerator/Application.java
@@ -1,5 +1,7 @@
 package com.audreynanual.unittestgenerator;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -10,24 +12,18 @@ import java.util.Scanner;
 public class Application {
     public static void main(String[] args) throws IOException, InterruptedException {
 
-        // TODO: Accept user input for the function they want to generate a unit test for
+        // Accept user input for the function they want to generate a unit test for
         System.out.println("==================== CLI Unit Test Generator ====================");
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the function you want to generate a unit test for: ");
         String searchString = scanner.nextLine();
 
-        // %s is a string format specifier that serves as a placeholder for a value that will be substituted at runtime.
-        // Specifically, it is used as a placeholder for the value of the searchString variable, which is a string.
-        String input = """
-                {
-                  "model": "text-davinci-003",
-                  "prompt": "%s",
-                  "temperature": 0.3,
-                  "max_tokens": 100
-                }
-                """.formatted(searchString);
+        // Create a request object to send to the OpenAI API
+        ObjectMapper objectMapper = new ObjectMapper();
+        ApiRequest apiRequest = new ApiRequest("text-davinci-003", searchString, 0.3, 1000);
+        String input = objectMapper.writeValueAsString(apiRequest);
 
-        // TODO: Create a request to the OpenAI API
+        // Create a request to send to the OpenAI API
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create("https://api.openai.com/v1/completions"))
                 .header("Content-Type", "application/json")
@@ -35,7 +31,7 @@ public class Application {
                 .POST(HttpRequest.BodyPublishers.ofString(input))
                 .build();
 
-        // TODO: Create a client to send the request to the OpenAI API
+        // Send the request to the OpenAI API
         HttpClient client = HttpClient.newHttpClient();
         var response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
