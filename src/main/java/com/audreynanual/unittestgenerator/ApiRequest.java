634f9605f261208eccc2ea48953ad2844ead0947
Audrey Carmel Jay J. Nanual <68633976+audreycj@users.noreply.github.com>
2023-04-27 09:48:00 +0800

    style: add whitespace

--- src/main/java/com/audreynanual/unittestgenerator/ApiRequest.java
+++ src/main/java/com/audreynanual/unittestgenerator/ApiRequest.java
@@ -3,8 +3,4 @@ package com.audreynanual.unittestgenerator;
 // This record represents what a request looks like.
 public record ApiRequest(String model, String prompt, double temperature, int max_tokens) {
 
-    // This is a constructor that takes in the values for the record.
-    // It is a shorthand way of writing a constructor.
-    // The constructor is used to create an instance of the record.
-    // The constructor is used in the Application.java file.
 }
