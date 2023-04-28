dc3b0fabefbec547b06734e1512d85efe22a31fc
Libing Chen <libing.chen@gmail.com>
2023-04-28 11:44:10 +0800

    chore: introduce @SpringBootApplication

--- src/main/java/com/audreynanual/unittestgenerator/Application.java
+++ src/main/java/com/audreynanual/unittestgenerator/Application.java
@@ -1,22 +1,12 @@
 package com.audreynanual.unittestgenerator;
 
-import picocli.CommandLine;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application implements Runnable {
-
-    @CommandLine.Mixin
-    private CommandLineOptions options;
+public class Application {
 
     public static void main(String[] args) {
-        int exitCode = new CommandLine(new Application()).execute(args);
-        System.exit(exitCode);
-    }
-
-    @Override
-    public void run() {
-        // Call the run method of the CommandLineOptions object
-        options.run();
+        System.exit(SpringApplication.exit(SpringApplication.run(Application.class, args)));
     }
 }
