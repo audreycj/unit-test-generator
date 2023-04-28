da3eb616fbae08e5aa44c2f031c7a93e1d2f28f0
Audrey Carmel Jay J. Nanual <68633976+audreycj@users.noreply.github.com>
2023-04-28 10:45:06 +0800

    docs: add comments

--- src/main/java/com/audreynanual/unittestgenerator/Application.java
+++ src/main/java/com/audreynanual/unittestgenerator/Application.java
@@ -6,14 +6,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application implements Runnable {
 
+    // The @Mixin annotation is used to add the CommandLineOptions object to the Application object
     @CommandLine.Mixin
     private CommandLineOptions options;
 
     public static void main(String[] args) {
+        // Call the execute method of the CommandLine object
         int exitCode = new CommandLine(new Application()).execute(args);
         System.exit(exitCode);
     }
 
+    // The run method is called when the command line options are parsed
     @Override
     public void run() {
         // Call the run method of the CommandLineOptions object
